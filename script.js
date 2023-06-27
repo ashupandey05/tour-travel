7a6b883ff2c5b2af19ec68932822ff7425b87db3
Ashutosh <ashutosh.ashu.0520@gmail.com>
2023-06-27 09:45:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+    strings: ['Get the best deal for your best holidays.', 'Plan your Indian and International travel with us!!!'],
+    typeSpeed: 70,
+    loop: true,
+  });
+  var typed2 = new Typed('#autotype', {
+    strings: ['Send your query here!'],
+    typeSpeed: 90,
+    loop: true,
+  });
